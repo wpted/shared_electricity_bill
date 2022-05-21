6b0dfc6a63990a533f1b914c5e91ca1b6ea65a44
wpted <silverhandsome@gmail.com>
2022-05-21 18:12:51 +0800

    Add webbrowser for automatically open the pdf file after generation

--- pdf_report.py
+++ pdf_report.py
@@ -1,4 +1,6 @@
 from fpdf import FPDF
+import webbrowser
+import os.path
 from bill import Bill
 
 
@@ -20,19 +22,26 @@ class PDFReport:
         pdf = FPDF(orientation="P", unit="pt", format="A4")
         pdf.add_page()
 
+        # Add Icon
+        pdf.image(name="house_icon.png", w=40, h=40)
+
         # Insert title
-        pdf.set_font(family="Times", size=24, style="B")
+        pdf.set_font(family="Courier", size=24, style="B")
         pdf.cell(w=100, h=40, ln=1, align="c", txt="Shared Electricity Bill")
 
 
         # Insert Period
-        pdf.cell(w=100, h=40, txt="period:")
+        pdf.set_font(family="Courier", size=12)
+        pdf.cell(w=100, h=40, txt="Period:")
         pdf.cell(w=150, h=40, txt=bill.period, ln=1)
 
         # Insert Flatmate
         for flatmate in flatmate_list:
+            pdf.set_font(family="Courier", size=12)
             pdf.cell(w=100, h=40, txt=flatmate.name)
             pdf.cell(w=150, h=40, ln=1, txt=str(flatmate.pays(bill, flatmate_list)))
 
         pdf.output(self.filename)
+        webbrowser.open("file://" + os.path.realpath("bill.pdf"))
+
 
